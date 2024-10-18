26a6b8b500df064aa3eefd1873fd221449f73629
yuva-1705 <130533625+yuva-1705@users.noreply.github.com>
2024-10-18 12:15:20 -0400

    Add files via upload
    Week 1 HM

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,18 @@
+package homeAssignment;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i=10;
+    if(i>5)
+    {
+    	System.out.println("The number is Positive");
+    }
+    else 
+    {
+    	System.out.println("The number is Negative");
+	}
+
+}
+}
